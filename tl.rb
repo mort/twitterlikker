9874e5a4856023972cda543f9108512f4b6cae50
mort <manuel@simplelogica.net>
2009-03-19 11:52:47 +0100

    first commit

---
+++ tl.rb
@@ -0,0 +1,56 @@
+require 'rubygems'
+require 'sinatra'
+require 'dm-core'
+require 'json'
+require 'cgi'
+
+DataMapper.setup(:default, :host => 'localhost', :adapter => 'mysql', :username => 'root', :database => 'twitterlikker')
+  
+class Liking
+  include DataMapper::Resource
+  property :id, Serial
+  property :who, String, :index => :unique
+  property :permalink, String, :index => :unique
+  property :created_at, DateTime
+end
+
+DataMapper::Logger.new(STDOUT, :debug)
+#DataMapper.auto_upgrade!
+
+post '/likings' do
+  @liking = Liking.new
+  @liking.who = params[:who]
+  @liking.permalink = params[:permalink]
+  @liking.created_at = Time.now
+  
+  if @liking.save
+    status(201)
+  end
+  
+end
+
+
+get '/likings' do
+  puts params.inspect
+  status(404) unless params[:p]
+  
+  permalinks = params[:p].split(',')
+  response = {}
+  
+  permalinks.each do |p|
+    #p = CGI::escape(p)
+    results = Liking.all(Liking.permalink => p)
+    unless results.empty?
+      b = Array.new
+      results.each do |r|
+        b << r.who
+      end
+      response[p] = b
+    end
+  end
+  
+    mime :json, "application/json"
+    content_type :json
+    response.to_json unless response.empty?  
+  
+end
